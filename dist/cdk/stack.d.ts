b09a5eb49627a6c170992bb565057cc54d41e814
Robert Grigorian <dev.robert.grigoryan@gmail.com>
2024-01-24 00:02:23 +0400

    fix: [VP-1] - bundle everything

--- dist/cdk/stack.d.ts
+++ dist/cdk/stack.d.ts
@@ -2,21 +2,22 @@ import type { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 type Props = cdk.StackProps & {
+    environment: string;
     domain?: string;
     path?: string;
     project?: string;
     priceClass?: cloudfront.PriceClass;
-    variables?: Record<string, string | undefined>;
     identity?: string;
 };
 export declare class Stack extends cdk.Stack {
-    constructor(scope: Construct, id: string, { path, variables, domain, project, priceClass, identity, ...props }: Props);
+    constructor(scope: Construct, id: string, { environment, path, domain, project, priceClass, identity, ...props }: Props);
     private getOriginAccessIdentity;
     private getBucket;
     private getZone;
     private getCertificate;
     private getOrigin;
     private getDistribution;
+    private createEnvSpecificOrigin;
     private createSubdomainRecords;
     private createOutput;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,47 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { execSync } from 'child_process';
+import { execSync } from 'child_process'
 
 export async function run(): Promise<void> {
   try {
-    const project = core.getInput('name');
-    const environment = core.getInput('environment');
-    const variables = JSON.parse(core.getInput('variables') || '{}');
-    const action = core.getInput('action') as 'deploy' | 'destroy';
-    const pr = github.context?.payload?.pull_request?.number;
-    const folder = github.context?.sha.slice(0, 7);
-    const domain = variables?.DOMAIN;
-    const stack = pr ? `${pr}-${environment}-${project}` : `${environment}-${project}`;
-    const vars = [];
+    const project = core.getInput('name')
+    const environment = core.getInput('environment')
+    const variables = JSON.parse(core.getInput('variables') || '{}')
+    const action = core.getInput('action') as 'deploy' | 'destroy'
+    const pr = github.context?.payload?.pull_request?.number
+    const folder = github.context?.sha.slice(0, 7)
+    const domain = variables?.DOMAIN
+    const vars = []
+    const stack = pr
+      ? `${pr}-${environment}-${project}`
+      : `${environment}-${project}`
 
-    if (!domain) throw new Error('Missing DOMAIN variable');
-    if (!project) throw new Error('Missing name input');
-    if (!environment) throw new Error('Missing environment input');
-    if (!action) throw new Error('Missing action input');
+    if (!domain) throw new Error('Missing DOMAIN variable')
+    if (!project) throw new Error('Missing name input')
+    if (!environment) throw new Error('Missing environment input')
+    if (!action) throw new Error('Missing action input')
 
     for (const [key, value] of Object.entries(variables)) {
-      vars.push(`${key}=${value}`);
+      vars.push(`${key}=${value}`)
     }
 
-    execSync(`echo "${vars.join('\n')}" > .env`);
+    execSync(`echo "${vars.join('\n')}" > .env`)
 
-    core.debug(execSync('cat .env').toString());
+    core.debug(execSync('cat .env').toString())
 
-    execSync(`npx cdk ${action} ${stack} --require-approval never --outputs-file cdk-outputs.json`);
+    execSync(
+      `npx cdk ${action} ${stack} --require-approval never --outputs-file cdk-outputs.json`
+    )
 
-    const outputs = JSON.parse(execSync(`cat ./cdk-outputs.json`).toString());
+    const outputs = JSON.parse(execSync(`cat ./cdk-outputs.json`).toString())
 
-    core.debug(JSON.stringify(outputs, null, 2));
+    core.debug(JSON.stringify(outputs, null, 2))
 
     // Set outputs for other workflow steps to use
-    core.setOutput('folder', folder);
-    core.setOutput('bucket', outputs[stack].BucketName);
-    core.setOutput('id', outputs[stack].DistributionId);
-    core.setOutput('url', outputs[stack].DeploymentUrl);
+    core.setOutput('folder', folder)
+    core.setOutput('bucket', outputs[stack].BucketName)
+    core.setOutput('id', outputs[stack].DistributionId)
+    core.setOutput('url', outputs[stack].DeploymentUrl)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
