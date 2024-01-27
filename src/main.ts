a2a74cbc5df8ccbda85f652b29a0b0301c12165b
Robert Grigorian <dev.robert.grigoryan@gmail.com>
2024-01-28 02:33:50 +0400

    fix: [VP-1] - debug env variables

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ const CDK_ARGS = `--require-approval never --outputs-file cdk-outputs.json`;
 
 export async function run(): Promise<void> {
   try {
+    core.debug(`>>> ENV:\n${JSON.stringify(process.env)}`);
+
     const { vars, stack, action, folder } = getInputs();
 
     execSync(`echo "${vars.join('\n')}" > .env`);
     core.debug(`>>> .env:\n${execSync(`cat .env`).toString()}`);
-
-    core.debug(`>>> List of files:\n${execSync(`ls -la`).toString()}`);
 
     execSync(`echo $CDK_CONFIG > ~/.cdk.json`);
     core.debug(`>>> cdk.json:\n${execSync(`cat cdk.json`).toString()}`);
