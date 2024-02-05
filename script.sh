ffc4149ddbb579b754e6af4dbbd9c76ca7ecb2a0
Robert Grigorian <dev.robert.grigoryan@gmail.com>
2024-02-05 22:28:03 +0400

    fix: [VP-1] - remove quotes instantly

--- script.sh
+++ script.sh
@@ -15,38 +15,35 @@ echo ">>> STACK -> $STACK"
 if [ $CDK_ACTION = "destroy" ]; then
   echo ">>> Destroying the stack"
   npx cdk destroy $STACK --force --require-approval never
-  exit 0
-fi
-
-echo ">>> Deploying the stack"
-npx cdk $CDK_ACTION $STACK --require-approval never --ci --outputs-file cdk-outputs.json
-
-# Return the result of the deployment
-BUCKET=$(jq --arg key "$STACK" '.[$key].BucketName' cdk-outputs.json)
-echo "bucket=$(echo $BUCKET | sed -e 's/"//g')" >> $GITHUB_OUTPUT
-echo "id=$(jq --arg key "$STACK" '.[$key].DistributionId' cdk-outputs.json)" >> $GITHUB_OUTPUT
-URL=$(jq --arg key "$STACK" '.[$key].DeploymentUrl' cdk-outputs.json)
-echo "url=$(echo $URL | sed -e 's/"//g')" >> $GITHUB_OUTPUT
-
-# Print the outputs
-echo "RESULT >>> cdk-outputs.json ->"
-cat cdk-outputs.json
-
-# Create the env.json file
-echo $VARIABLES | \
-jq 'with_entries(select(.key | startswith("STACK_") | not))' | \
-jq 'with_entries(select(.key | startswith("CLIENT_")) | .key |= sub("^CLIENT_"; ""))' \
-> env.json
-
-echo ">>> env.json ->"
-cat env.json
-
-if [ $ENVIRONMENT = "tmp" ]; then
-  echo ">>> Deploying the env variables"
-  aws s3 cp env.json s3://$BUCKET/$GITHUB_REF_NAME/env.json
 else
-  echo ">>> Deploying the env variables to $ENVIRONMENT"
-  aws s3 cp env.json s3://$BUCKET/$ENVIRONMENT/env.json
-fi
+  echo ">>> Deploying the stack"
+  npx cdk $CDK_ACTION $STACK --require-approval never --ci --outputs-file cdk-outputs.json
 
-exit 0
+  # Return the result of the deployment
+  BUCKET=$(jq --arg key "$STACK" '.[$key].BucketName' cdk-outputs.json) | sed -e 's/"//g'
+  echo "bucket=$BUCKET" >> $GITHUB_OUTPUT
+  echo "id=$(jq --arg key "$STACK" '.[$key].DistributionId' cdk-outputs.json)" >> $GITHUB_OUTPUT
+  URL=$(jq --arg key "$STACK" '.[$key].DeploymentUrl' cdk-outputs.json) | sed -e 's/"//g'
+  echo "url=$URL" >> $GITHUB_OUTPUT
+
+  # Print the outputs
+  echo "RESULT >>> cdk-outputs.json ->"
+  cat cdk-outputs.json
+
+  # Create the env.json file
+  echo $VARIABLES | \
+  jq 'with_entries(select(.key | startswith("STACK_") | not))' | \
+  jq 'with_entries(select(.key | startswith("CLIENT_")) | .key |= sub("^CLIENT_"; ""))' \
+  > env.json
+
+  echo ">>> env.json ->"
+  cat env.json
+
+  if [ $ENVIRONMENT = "tmp" ]; then
+    echo ">>> Deploying the env variables"
+    aws s3 cp env.json s3://$BUCKET/$GITHUB_REF_NAME/env.json
+  else
+    echo ">>> Deploying the env variables to $ENVIRONMENT"
+    aws s3 cp env.json s3://$BUCKET/$ENVIRONMENT/env.json
+  fi
+fi
