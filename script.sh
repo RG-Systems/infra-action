350d2b13511b404d17e31730af5d6352f97fac51
Robert Grigorian <dev.robert.grigoryan@gmail.com>
2024-02-05 20:41:01 +0400

    fix: [VP-1] - try to see variables

--- script.sh
+++ script.sh
@@ -11,12 +11,25 @@ fi
 
 echo "RESULT >>> STACK -> $STACK"
 
-# Deploy the infrastructure
-npx cdk deploy $STACK --require-approval never --outputs-file cdk-outputs.json
-BUCKET=$(jq --arg key "$STACK" '.[$key].BucketName' cdk-outputs.json)
-echo "bucket=$(echo $BUCKET | sed -e 's/"//g')" >> $GITHUB_OUTPUT
-echo "id=$(jq --arg key "$STACK" '.[$key].DistributionId' cdk-outputs.json)" >> $GITHUB_OUTPUT
-URL=$(jq --arg key "$STACK" '.[$key].DeploymentUrl' cdk-outputs.json)
-echo "url=$(echo $URL | sed -e 's/"//g')" >> $GITHUB_OUTPUT
-echo "RESULT >>>"
-cat cdk-outputs.json
+# Check if the stack exists else create it
+if [ $CDK_ACTION = "destroy" ]; then
+  echo "RESULT >>> Destroying the stack"
+  npx cdk destroy $STACK --force --require-approval never
+  exit 0
+else
+  npx cdk $CDK_ACTION $STACK --require-approval never --ci --outputs-file cdk-outputs.json
+
+  # Return the result of the deployment
+  BUCKET=$(jq --arg key "$STACK" '.[$key].BucketName' cdk-outputs.json)
+  echo "bucket=$(echo $BUCKET | sed -e 's/"//g')" >> $GITHUB_OUTPUT
+  echo "id=$(jq --arg key "$STACK" '.[$key].DistributionId' cdk-outputs.json)" >> $GITHUB_OUTPUT
+  URL=$(jq --arg key "$STACK" '.[$key].DeploymentUrl' cdk-outputs.json)
+  echo "url=$(echo $URL | sed -e 's/"//g')" >> $GITHUB_OUTPUT
+
+  # Print the outputs
+  echo "RESULT >>> cdk-outputs.json ->"
+  cat cdk-outputs.json
+  exit 0
+fi
+
+echo "VARIABLES: $VARIABLES"
